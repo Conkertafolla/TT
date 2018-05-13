1c91e375d2071bf99016a9f28c0ed88887acff30
Conkertafolla <30909174+Conkertafolla@users.noreply.github.com>
2018-05-13 15:21:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/conke/tt/menu.java
@@ -0,0 +1,14 @@
+package com.example.conke.tt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Menu principal");
+        setContentView(R.layout.activity_menu);
+    }
+}
