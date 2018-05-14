61a6c0c88fe38a996671170865d4e1b3dd9031a0
Conkertafolla <30909174+Conkertafolla@users.noreply.github.com>
2018-05-13 21:10:53 -0500

    Calendario y material desing

--- app/src/main/java/com/example/conke/tt/registrarAdulto.java
+++ app/src/main/java/com/example/conke/tt/registrarAdulto.java
@@ -9,35 +9,53 @@ import android.view.View;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.Calendar;
 
-public class registrarAdulto extends AppCompatActivity {
-    EditText fechaNac;
-    private int dia,mes,ano;
+public class registrarAdulto extends AppCompatActivity implements View.OnClickListener{
+    private static final String CERO = "0";
+    private static final String BARRA = "/";
+    public final Calendar c = Calendar.getInstance();
+
+    final int dia= c.get(Calendar.DAY_OF_MONTH);
+    final int mes= c.get(Calendar.MONTH);
+    final int ano= c.get(Calendar.YEAR);
+
+    EditText etFecha;
+    ImageButton obtenerFecha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        fechaNac=findViewById(R.id.fecha_AM);
-        fechaNac.setOnClickListener((View.OnClickListener) this);
         setContentView(R.layout.activity_registrar_adulto);
+        etFecha=(EditText) findViewById(R.id.fecha_AM);
+        obtenerFecha=(ImageButton) findViewById(R.id.selFec);
+        obtenerFecha.setOnClickListener(this);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public void onClick(View v){
-        if(v==fechaNac){
-            final Calendar c =Calendar.getInstance();
-            dia=c.get(Calendar.DAY_OF_MONTH);
-            mes=c.get(Calendar.MONTH);
-            ano=c.get(Calendar.YEAR);
-
-            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener(){
-                @Override
-                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                    fechaNac.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
-                }
-            },dia,mes,ano);
-            datePickerDialog.show();
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.selFec:
+                obtenerFecha();
+                break;
         }
     }
+
+    private void obtenerFecha(){
+        DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                final int mesActual = month+1;
+                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+                etFecha.setText(diaFormateado + BARRA + mesFormateado + BARRA + year);
+            }
+        },ano,mes,dia);
+        recogerFecha.show();
+    }
+
+
+
+
 }
